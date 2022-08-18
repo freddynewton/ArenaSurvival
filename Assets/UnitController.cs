753dde2df85b68a96d426e92b28250382ebcb8c1
Fred Newton <ghostlopment@gmail.com>
2022-08-18 18:41:25 +0200

    kinda working ai movement

---
+++ Assets/UnitController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner;
+using BehaviorDesigner.Runtime.Tactical;
+
+namespace freddynewton
+{
+    public class UnitController : MonoBehaviour, IAttackAgent
+    {
+        public void Attack(Vector3 targetPosition)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public float AttackAngle()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public float AttackDistance()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CanAttack()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
